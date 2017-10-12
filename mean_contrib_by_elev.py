41114fc46f520718f117845fc965ff04c5619d39
Bruce Raup <bruceraup@gmail.com>
2017-10-12 15:40:08 -0600

    Rearranged order of operations

--- mean_contrib_by_elev.py
+++ mean_contrib_by_elev.py
@@ -8,28 +8,35 @@ import pandas as pd
 
 import hypsometry_utils as hu
 
+
+def melt_by_elev(year, typ):
+    """
+    Calculate a table of melt contribution by elevation band, given a year and
+    a source type.
+    """
+    name_patt = "*.{year}.*{typ}_melt_by_elev.best_Hunza_model.txt".format(year=year, typ=typ)
+    filelist = glob.glob(topdir + name_patt)
+    if len(filelist) > 1:
+        print("WARNING: more than one file matching: ", name_patt)
+        print("Using first one")
+    fn = filelist[0]
+    hyps = hu.Hypsometry(filename=fn)
+    elev_sum = hyps.sum_by_elevband()
+    return elev_sum
+
+
 topdir = '/projects/CHARIS/derived_hypsometries/MODSCAG_GF_v09_fromFile_rainv01_less_ET/IN_Hunza_at_DainyorBridge/'
 
 source_types = ['exposed_glacier_ice', 'snow_on_ice', 'snow_on_land']
-srctype_re = '|'.join(source_types)
 
-egi_by_year = {}
-sol_by_year = {}
-soi_by_year = {}
-#for year in range(2001, 2014 + 1):
-for year in range(2001, 2001 + 1):
-    name_patt = "*.{year}.*_melt_by_elev.best_Hunza_model.txt".format(year=year)
-    filelist = glob.glob(topdir + name_patt)
+for typ in source_types:
+    year_range = range(2001, 2014 + 1)
+    year_range = range(2001, 2001 + 1)
+    for year in year_range:
+        melt = melt_by_elev(year, typ)
+        print("{}, {}".format(typ, year))
+        print(melt)
 
-    print("Processing year {year} ...".format(year=year))
-    for fn in filelist:
-        type_ = re.search(srctype_re, fn).group(0)
-        hyps = hu.Hypsometry(filename=fn)
-        #hyps.print()
-        elev_sum = hyps.sum_by_elevband()
-        sums_by_year[year] = elev_sum
-        print("    **** {} ****".format(type_), fn)
-        print("    ", elev_sum)
 
 # Example set of files:
 # 
