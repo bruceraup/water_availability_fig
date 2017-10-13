977e1b2154052b0a8e0cf78f8359077be5b0b7b8
Bruce Raup <bruceraup@gmail.com>
2017-10-13 10:32:59 -0600

    Works now summing all subbasins. Need to rebin to 500 m bins

--- mean_contrib_by_elev.py
+++ mean_contrib_by_elev.py
@@ -3,6 +3,7 @@
 import sys
 import re
 import glob
+import os
 
 import pandas as pd
 
@@ -15,7 +16,7 @@ def melt_by_elev(topdir, year, typ):
     a source type.
     """
     name_patt = "*.{year}.*{typ}_melt_by_elev.best_Hunza_model.txt".format(year=year, typ=typ)
-    filelist = glob.glob(topdir + name_patt)
+    filelist = glob.glob(os.path.join(topdir, name_patt))
     if len(filelist) > 1:
         print("WARNING: more than one file matching: ", name_patt)
         print("Using first one")
@@ -35,23 +36,38 @@ def do_one_basin(topdir):
     for typ in source_types:
         year_range = range(startyear, endyear + 1)
         mean_contrib = pd.concat([melt_by_elev(topdir, yr, typ) for yr in year_range], axis=1).mean(axis=1)
-        #print("=== Type {} ===".format(typ))
-        #print(mean_contrib)
         if contribs_all is None:
             contribs_all = mean_contrib
         else:
             contribs_all = pd.concat((contribs_all, mean_contrib), axis=1)
 
     contribs_all.columns = source_types
-    with pd.option_context('display.max_rows', None):
-        print(contribs_all)
-    #contribs_all.to_csv('mean_contribs_by_type.csv')
+    contribs_all = contribs_all.fillna(value=0)
+    return contribs_all
 
 
 def main():
 
-    topdir = '/projects/CHARIS/derived_hypsometries/MODSCAG_GF_v09_fromFile_rainv01_less_ET/IN_Hunza_at_DainyorBridge/'
-    do_one_basin(topdir)
+    topdir = '/projects/CHARIS/derived_hypsometries/MODSCAG_GF_v09_fromFile_rainv01_less_ET/'
+    subdir_patt = 'IN_v01_OBJECTID*'
+    subdirs = glob.glob(os.path.join(topdir, subdir_patt))
+
+    print("Subdirs:  ", subdirs)
+
+    contribs_fullbasin = None
+
+    for subdir in subdirs:
+        contribs_onebasin = do_one_basin(subdir)
+        print("======== SUBDIR: ", subdir)
+        #print(contribs_onebasin)
+        if contribs_fullbasin is None:
+            contribs_fullbasin = contribs_onebasin
+        else:
+            contribs_fullbasin = contribs_fullbasin.add(contribs_onebasin, fill_value=0.0)
+
+    with pd.option_context('display.max_rows', None):
+        print(contribs_fullbasin)
+    #contribs_all.to_csv('mean_contribs_by_type.csv')
 
 
 if __name__ == '__main__':
